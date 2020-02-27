950d3a62ae250849c0a83e2e42c94136b92635c4
Nitesh Kesarkar <nitesh_k@techjoomla.com>
2020-02-27 15:47:13 +0530

    Added contribute module

---
+++ src/app/client/src/app/modules/contribute/contribute.module.ts
@@ -0,0 +1,46 @@
+import { CoreModule } from '@sunbird/core';
+import { SharedModule } from '@sunbird/shared';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SuiModule } from 'ng2-semantic-ui/dist';
+import { SlickModule } from 'ngx-slick';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ContributeRoutingModule } from './contibute-routing.module';
+import { ListAllProgramsComponent } from './components';
+import { ProgramComponent } from './components/program/program.component';
+import { OnboardPopupComponent } from './components/onboard-popup/onboard-popup.component';
+import { ProgramHeaderComponent } from './components/program-header/program-header.component';
+import { NgInviewModule } from 'angular-inport';
+import { TelemetryModule } from '@sunbird/telemetry';
+import { DynamicModule } from 'ng-dynamic-component';
+import { CbseProgramModule } from '../../modules/cbse-program/cbse-program.module';
+import { CollectionComponent, DashboardComponent } from '../cbse-program';
+import { CommonConsumptionModule} from '@project-sunbird/common-consumption';
+import { EnrollContributorComponent } from './components/enroll-contributor/enroll-contributor.component';
+import { ListAllMyProgramsComponent } from './components/list-all-my-programs/list-all-my-programs.component';
+@NgModule({
+  
+  declarations: [ListAllProgramsComponent, ProgramComponent, OnboardPopupComponent, ProgramHeaderComponent,EnrollContributorComponent, ListAllMyProgramsComponent  ],
+  imports: [
+    SuiModule,
+    CommonModule,
+    ContributeRoutingModule,
+    CommonConsumptionModule,
+    SlickModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    CoreModule,
+    NgInviewModule,
+    TelemetryModule,
+    CbseProgramModule,
+    
+    DynamicModule.withComponents([CollectionComponent, DashboardComponent])
+  ],
+  exports: [
+    ProgramComponent,
+    OnboardPopupComponent,
+  ]
+})
+export class ContributeModule { }
